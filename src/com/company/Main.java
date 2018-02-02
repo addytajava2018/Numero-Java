9d16e1f46b8ccf9b24afa4d5d3d83d9b62056feb
addyta <adriananp41@gmail.com>
2018-02-01 20:42:15 -0400

    Numeros en java

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // para mostrar el system ctrl+j escribir sout
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero:");
+        String valor = sc.nextLine();//ingresa como string
+        int a = Integer.parseInt(valor);
+        Numero n = new Numero(a);
+        if (n.esPar())
+            System.out.println("El numero " + a + " es par.");
+        else
+            System.out.println("El numero " + a + " es impar.");
+    }
+}
---
+++ src/com/company/Numero.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Numero {
+    int num;
+
+    public Numero(int num){
+        this.num=num;
+    }
+
+    public boolean esPar(){
+        return num%2==0;
+    }
+
+    public boolean esPrimo(){
+
+    }
+
+}
